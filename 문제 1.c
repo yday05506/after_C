49e1880c173123890948f9c782ed5de28b8ebe81
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2022-01-04 14:11:20 +0900

    교수님이 만져준 프로그램

--- 문제 1.c
+++ 문제 1.c
@@ -6,17 +6,18 @@ void main(void)
 	int b;
 	int sum = 0;
 
-	while (a != 0) {
+	while (1) {
 		printf("0~1000 사이의 정수 입력 : ");
 		scanf("%d", &a);
-		if (a % 2 == 0) {
+		if (!a) break;
+		if (!(a % 2 )) {
 			b = a * a * a;
 			printf("%d\n", b);
 		}
-		else if (a % 2 == 1) {
+		else if (a % 2 ) {
 			if(a % 2 == 1)
 				sum += a;
 			printf("%d\n", sum);
 		}
 	}
-}
+}       
